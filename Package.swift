c8d48374c60e51fd0125ce59723d49ce5650a685
Gui Rambo <insidegui@gmail.com>
2024-03-13 11:23:57 -0300

    Removed all platforms except macOS, raised deployment target, enabled new features

--- Package.swift
+++ Package.swift
@@ -1,14 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "whisper",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v14),
-        .watchOS(.v4),
-        .tvOS(.v14)
+        .macOS(.v14)
     ],
     products: [
         .library(name: "whisper", targets: ["whisper"]),
@@ -45,12 +42,12 @@ let package = Package(
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-                .define("GGML_USE_METAL")
+                .define("GGML_USE_METAL"),
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
-                // .define("ACCELERATE_NEW_LAPACK"),
-                // .define("ACCELERATE_LAPACK_ILP64")
+                 .define("ACCELERATE_NEW_LAPACK"),
+                 .define("ACCELERATE_LAPACK_ILP64")
             ],
             linkerSettings: [
                 .linkedFramework("Accelerate")
